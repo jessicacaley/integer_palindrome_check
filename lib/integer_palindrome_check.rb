fac57939c61cf3456fd0edfb0712ac30b2a9fcd2
Jessica <jessica.homet@gmail.com>
2019-03-28 01:33:36 -0700

    Implemented is_palindrome and helper method number_length

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,6 +1,42 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the length of the number in digits
+# Space complexity: O(1)?
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil || number < 0
+
+  right_num_divisor = 1
+  right_num_modulo = 10
+  left_num_divisor = 10 ** (number_length(number) - 1)
+
+  (number_length(number) / 2).times do
+    left_num = number / left_num_divisor % 10
+    right_num = number % right_num_modulo / right_num_divisor
+
+    return false unless left_num == right_num
+
+    left_num_divisor /= 10
+    right_num_divisor *= 10
+    right_num_modulo *= 10
+  end
+
+  return true
+end
+
+def number_length(number)
+  divisor = 1
+  digits = 0
+  quotient = number % divisor
+  previous_quotient = nil
+  until previous_quotient == quotient
+    digits += 1
+    previous_quotient = number % divisor
+    divisor *= 10
+    quotient = number % divisor
+  end
+
+  # go to last one
+  digits -= 1
+  divisor /= 10
+
+  return digits
 end
